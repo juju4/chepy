481a6bdb52b498d6e3d9edab9ac39dc475418e92
juju4 <juju4@users.noreply.github.com>
2026-08-30 21:27:55 +0000

    fix: syntax gen-msix.py

--- gen-msix.py
+++ gen-msix.py
@@ -3,10 +3,11 @@ Script to generate msix from Linux
 
 Requirements: pymsix and makemsix
 """
+
 from msix import MsixPacker
 
 packer = MsixPacker()
-app_name = chepy
+app_name = "chepy"
 packer.pack("dist/", f"output/{app_name}.msix")
 packer.unpack(f"output/{app_name}.msix", "/tmp/extracted/")
 packer.sign(
