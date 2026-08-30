ef2595135fec355114b01b609f3ee47ccdb270ba
juju4 <juju4@users.noreply.github.com>
2026-08-30 20:08:12 +0000

    feat: msix generation with pymsix

---
+++ gen-msix.py
@@ -0,0 +1,17 @@
+"""
+Script to generate msix from Linux
+
+Requirements: pymsix and makemsix
+"""
+from msix import MsixPacker
+
+packer = MsixPacker()
+app_name = chepy
+packer.pack("dist/", f"output/{app_name}.msix")
+packer.unpack(f"output/{app_name}.msix", "/tmp/extracted/")
+packer.sign(
+    f"output/{app_name}.msix",
+    "certificate.pfx",
+    pfx_password="test",
+    timestamp_url="http://timestamp.digicert.com",
+)
