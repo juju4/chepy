82f9ba7e31a8b8ee84051eb8fea5a33d1a478746
juju4 <juju4@users.noreply.github.com>
2026-09-13 18:28:38 +0000

    ci(pyinstaller): create pymsix output folder

--- gen-msix.py
+++ gen-msix.py
@@ -4,14 +4,28 @@ Script to generate msix from Linux
 Requirements: pymsix and makemsix
 """
 
-from msix import MsixPacker
+from pathlib import Path
+from msix import MsixPacker  # type: ignore
 
 packer = MsixPacker(verbose=True)
-app_name = "chepy"
-packer.pack("dist/chepy", f"output/{app_name}.msix")
-packer.unpack(f"output/{app_name}.msix", "/tmp/extracted/")
+APP_NAME = "chepy"
+OUTPUT_DIR = "output"
+
+output_path = Path(OUTPUT_DIR)
+try:
+    output_path.mkdir()
+except FileExistsError:
+    print(f"Directory '{output_path}' already exists.")
+except PermissionError:
+    print(f"Permission denied: Unable to create '{output_path}'.")
+# pylint: disable=W0718
+except Exception as exc:
+    print(f"An error occurred: {exc}")
+
+packer.pack("dist/chepy", f"{OUTPUT_DIR}/{APP_NAME}.msix")
+packer.unpack(f"output/{APP_NAME}.msix", "/tmp/extracted/")
 packer.sign(
-    f"output/{app_name}.msix",
+    f"{OUTPUT_DIR}/{APP_NAME}.msix",
     "certificate.pfx",
     pfx_password="test",
     timestamp_url="http://timestamp.digicert.com",
